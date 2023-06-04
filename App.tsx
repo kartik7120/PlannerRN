34eeae0d8526d8444f1f72d632139fd8b7d514c6
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-04 08:35:46 +0530

    Added Stack Navigator from react navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 /// <reference types="nativewind/types" />
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={() => <View><Text>Home</Text></View>} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 

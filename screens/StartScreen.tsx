db28cee4159480215eeab936fb9405f4f0ff5eb0
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-04 13:11:57 +0530

    Added a start screen component

--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -2,6 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import { Image } from '@rneui/themed';
+import { Button } from '@rneui/themed';
+import ButtonTitle from '../components/ButtonTitle';
 
 export default function StartScreen() {
     return (
@@ -10,6 +13,14 @@ export default function StartScreen() {
             <Text className='text-lg text-center'>
                 Keeps track of tasks manage guest list, control expenses, invite helpers and more
             </Text>
+            {/* <Image source={require("../assets/partyArt.jpg")} style={{
+                aspectRatio: 1,
+                resizeMode: 'contain',
+                width: '100%',
+                height: '100%',
+            }} /> */}
+            {/* <Button title={<ButtonTitle />} icon={} /> */}
+            <Image source={require("../assets/carnivalMask.svg")} />
             <StatusBar style="dark" />
         </SafeAreaView>
     )

e1b0330013555ef3724873006f59fd97c8a6348a
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-04 13:13:16 +0530

    Added a components folder

---
+++ components/ButtonTitle.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ButtonTitle() {
+    return (
+        <View className='flex flex-col gap-2'>
+            <Text className='text-xl text-white font-medium'>Start Planning</Text>
+            <Text className='text-sm text-white '>Create an account for free</Text>
+        </View>
+    )
+}

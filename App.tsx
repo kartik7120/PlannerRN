5bae59acb0f241d1e4d78e86436be7a1a789a232
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-04 11:20:19 +0530

    Wrapped the SafeAreaProvider around Stack Navigator

--- App.tsx
+++ App.tsx
@@ -1,31 +1,24 @@
 /// <reference types="nativewind/types" />
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import StartScreen from './screens/StartScreen';
 import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Start'>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Start" component={StartScreen} options={{
-          headerShown: false
-        }}/>
-      </Stack.Navigator>
+      <SafeAreaProvider>
+        <Stack.Navigator initialRouteName='Start'>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Start" component={StartScreen} options={{
+            headerShown: false
+          }} />
+        </Stack.Navigator>
+      </SafeAreaProvider>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

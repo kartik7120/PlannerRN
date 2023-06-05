ea8d64d5c6481c0c8096aa6d6e4ea2007eb04eef
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-05 17:00:02 +0530

    Added some flex styles to the Button Title container View

--- components/ButtonTitle.tsx
+++ components/ButtonTitle.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function ButtonTitle() {
     return (
-        <View className='flex flex-col gap-2'>
+        <View className='flex flex-col gap-1'>
             <Text className='text-xl text-white font-medium'>Start Planning</Text>
             <Text className='text-sm text-white '>Create an account for free</Text>
         </View>

17809f718aca845c0bac5b1f1052ebf27ec92f64
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-06 11:42:15 +0530

    Added firebase config files

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGYanpFwGlohrirpVZ-SVgyCyWVv0Rsb0",
+  authDomain: "planner-app-d5a1a.firebaseapp.com",
+  projectId: "planner-app-d5a1a",
+  storageBucket: "planner-app-d5a1a.appspot.com",
+  messagingSenderId: "88414469744",
+  appId: "1:88414469744:web:3815ed99052b331f457eb5",
+  measurementId: "G-HRN2TW119C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

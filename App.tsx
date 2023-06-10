c509c6906d000cca70f5af0a73cba5c5fe5235b4
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-10 13:05:23 +0530

    Added Tab navigation to the Home Screen

--- App.tsx
+++ App.tsx
@@ -3,13 +3,12 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import StartScreen from './screens/StartScreen';
-import HomeScreen from './screens/HomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { CLERK_PUBLISHABLE_KEY } from "@env"
 import * as SecureStore from "expo-secure-store";
 import StartNewScreen from './screens/StartNewScreen';
-import * as WebBrowser from "expo-web-browser";
+import TabNavigator from './screens/TabNavigator';
 
 export type RootStack = {
   Home: undefined;
@@ -42,8 +41,10 @@ export default function App() {
     <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.CLERK_PUBLISHABLE_KEY}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator initialRouteName='Start'>
-            <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name="Home" component={TabNavigator} options={{
+              headerShown: false
+            }}/>
             <Stack.Screen name="Start" component={StartScreen} options={{
               headerShown: false
             }} />

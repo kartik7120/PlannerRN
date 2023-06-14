16f3d26b4906a7bc093fd1834f8ee3e010ccb0ea
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-14 22:33:59 +0530

    Added Title to the upper tab

--- screens/TabBudgetScreen.tsx
+++ screens/TabBudgetScreen.tsx
@@ -49,34 +49,54 @@ export default function TabBudgetScreen() {
     }}>
       <TopTab.Screen name="Attire" component={TopTabEvent} initialParams={{
         title: 'Attire',
+      }} options={{
+        title: "Attire & Accessories"
       }} />
-      <TopTab.Screen name="Health" component={TopTabEvent} initialParams={{
+      <TopTab.Screen name="Health" component={TopTabEvent} options={{
+        title: "Health & Beauty"
+      }} initialParams={{
         title: 'Health',
       }} />
-      <TopTab.Screen name="Music" component={TopTabEvent} initialParams={{
+      <TopTab.Screen name="Music" options={{
+        title: "Music & Show"
+      }} component={TopTabEvent} initialParams={{
         title: 'Music',
       }} />
-      <TopTab.Screen name="Flower" component={TopTabEvent} initialParams={{
+      <TopTab.Screen name="Flower" options={{
+        title: "Flower & Decor"
+      }} component={TopTabEvent} initialParams={{
         title: 'Flower',
       }} />
-      <TopTab.Screen name="Photo" component={TopTabEvent} initialParams={{
+      <TopTab.Screen name="Photo" options={{
+        title: "Photo & Video"
+      }} component={TopTabEvent} initialParams={{
         title: 'Photo',
       }} />
-      <TopTab.Screen name="Accessories" component={TopTabEvent} initialParams={{
+      <TopTab.Screen name="Accessories" options={{
+        title: "Accessories"
+      }} component={TopTabEvent} initialParams={{
         title: 'Accessories',
       }} />
-      <TopTab.Screen name="Reception" component={TopTabEvent} initialParams={{
+      <TopTab.Screen name="Reception" options={{
+        title:"Reception"
+      }} component={TopTabEvent} initialParams={{
         title: 'Reception',
       }} />
       <TopTab.Screen name="Transpotation" component={TopTabEvent} initialParams={{
         title: 'Transpotation',
-      }} />
+      }}  options={{
+        title: "Transpotation"
+      }}/>
       <TopTab.Screen name="Accomodation" component={TopTabEvent} initialParams={{
         title: 'Accomodation',
-      }} />
+      }} options={{
+        title: "Accomodation"
+      }}/>
       <TopTab.Screen name="Unassigned" component={TopTabEvent} initialParams={{
         title: 'Unassigned',
-      }} />
+      }}  options={{
+        title: "Unassigned Category"
+      }}/>
     </TopTab.Navigator>
   )
 }

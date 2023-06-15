b8030a4ee2525694c0f311b1e916b7a7334e2680
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-15 13:36:55 +0530

    Added the FAB component to open add cost item

--- screens/TopTabEvent.tsx
+++ screens/TopTabEvent.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 import { Image } from '@rneui/themed';
 import { FAB } from '@rneui/themed';
 import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStack } from '../App';
+
+type TopTabEventNavigationProp =  StackNavigationProp<RootStack, 'BudgetModal'>;
 
 export default function TopTabEvent() {
 
   const [visible, setVisible] = React.useState(true);
+  const navigation = useNavigation<TopTabEventNavigationProp>();
 
   return (
     <View className='flex flex-col justify-center h-full items-center'>
@@ -17,7 +23,9 @@ export default function TopTabEvent() {
       }} />
       <Text className='text-black text-lg'>There are no cost</Text>
       <Text className='text-gray-500'>Click + to add a new item</Text>
-      <FAB className='mt-4' placement='right' color="orange" icon={<AntDesign name="plus" size={24} color="black" />} />
+      <FAB className='mt-4' onPress={() => {
+        navigation.navigate('BudgetModal');
+      }} placement='right' color="orange" icon={<AntDesign name="plus" size={24} color="black" />} />
     </View>
   )
 }

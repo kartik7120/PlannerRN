435c15fdddc2ddd197b2cb0f681c7f935a55f318
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-06-22 22:09:52 +0530

    Added a screen that will render all the details for a todo item

---
+++ screens/CheckListDetail.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CheckListDetail() {
+  return (
+    <View>
+      <Text>CheckListDetail</Text>
+    </View>
+  )
+}

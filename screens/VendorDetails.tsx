230927b15186f697e53b5f05cc724dcde33d2164
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-07-01 16:39:12 +0530

    Added a screen for rendering vendor details

---
+++ screens/VendorDetails.tsx
@@ -0,0 +1,105 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
+import { StackNavigationProp, } from '@react-navigation/stack';
+import { RootStack } from '../App';
+import { AntDesign } from '@expo/vector-icons';
+import { Divider, FAB } from '@rneui/themed';
+import { FontAwesome } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+type NavigationProps = StackNavigationProp<RootStack, 'VendorsDetailModal'>;
+type RProps = RouteProp<RootStack, "VendorsDetailModal">;
+
+export default function VendorDetails() {
+
+    const [vendorDetails, setVendorDetails] = useState<any>(null);
+    const navigation = useNavigation<NavigationProps>();
+    const route = useRoute<RProps>();
+
+    useEffect(() => {
+        async function getVendorDetail() {
+            const eventId = await AsyncStorage.getItem('currentEventId');
+            if (!eventId) return;
+            if (!route.params.id) return;
+            const docRef = doc(db, "events", eventId, "vendors", route.params.id);
+            const unsubscribe = onSnapshot(docRef, (doc) => {
+                setVendorDetails(doc.data());
+            })
+            return unsubscribe;
+        }
+        getVendorDetail();
+    }, [])
+
+    return (
+        <View>
+            <View className='bg-white m-2 p-2 rounded-lg border border-transparent overflow-hidden'>
+                <View className='flex flex-row items-center gap-x-4 mb-2'>
+                    <AntDesign name="infocirlceo" size={24} color="black" />
+                    <Text>DETAILS</Text>
+                </View>
+                <Divider width={1} />
+                <View className='flex flex-row justify-between items-center mt-2'>
+                    <View className='flex flex-col gap-y-4'>
+                        <Text>Name</Text>
+                        <Text>Note</Text>
+                        <Text>Category</Text>
+                        <Text>Status</Text>
+                    </View>
+                    <View className='flex flex-col gap-y-4'>
+                        <Text>{vendorDetails && vendorDetails.name || "Name is not defined"}</Text>
+                        <Text>{vendorDetails && vendorDetails.note || "Note is not defined"}</Text>
+                        <Text>{vendorDetails && vendorDetails.category || "Category is not defined"}</Text>
+                        <Text>{vendorDetails && vendorDetails.status || "Status is not defined"}</Text>
+                    </View>
+                </View>
+                <FAB color="orange" placement='right' icon={<AntDesign name="edit" size={24} color="white" />} onPress={() => {
+                    navigation.navigate("VendorsForm", {
+                        id: route.params.id,
+                        name: vendorDetails.name,
+                        note: vendorDetails.note,
+                        category: vendorDetails.category,
+                        phone: vendorDetails.phone,
+                        email: vendorDetails.email,
+                        website: vendorDetails.website,
+                        address: vendorDetails.address,
+                        amount: vendorDetails.amount,
+                        status: vendorDetails.status,
+                        edit: true
+                    })
+                }} />
+            </View>
+            <View className='bg-white m-2 p-2 rounded-lg border border-transparent overflow-hidden'>
+                <View className='flex flex-row items-center gap-x-4 mb-2'>
+                    <FontAwesome name="paper-plane-o" size={24} color="black" />
+                    <Text>CONTACTS</Text>
+                </View>
+                <Divider width={1} />
+                <View className='flex flex-row justify-between items-center mt-2'>
+                    <View className='flex flex-col gap-y-4'>
+                        <Text>Phone</Text>
+                        <Text>E-mail</Text>
+                        <Text>Site</Text>
+                        <Text>Address</Text>
+                    </View>
+                    <View className='flex flex-col gap-y-4'>
+                        <View className='flex flex-row items-center gap-x-2'>
+                            <Feather name="phone" size={20} color="black" />
+                            <Text>{vendorDetails && vendorDetails.phone.replace(/(\d{3})(\d{3})(\d{4})/, "($1)$2-$3") || "Phone is not defined"}</Text>
+                        </View>
+                        <View className='flex flex-row items-center gap-x-2'>
+                            <MaterialCommunityIcons name="email-outline" size={20} color="black" />
+                            <Text>{vendorDetails && vendorDetails.email || "Address is not defined"}</Text>
+                        </View>
+                        <Text>{vendorDetails && vendorDetails.site || "Site is not defined"}</Text>
+                        <Text>{vendorDetails && vendorDetails.address || "Address was not defined"} </Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    )
+}

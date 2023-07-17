a91b66a30882858181907345ee144bd59c587143
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-07-17 13:56:50 +0530

    Added some changes to thhe contact number component

--- components/ContactNumber.tsx
+++ components/ContactNumber.tsx
@@ -7,7 +7,6 @@ interface Props {
     image: string | null | undefined;
     name: string;
     phone: string | undefined;
-    setContact: (contact: Contacts.Contact | null) => void;
     id: string;
     isContactSet: boolean;
 }
@@ -19,43 +18,17 @@ export default function ContactNumber(props: Props) {
     const styles1 = "flex flex-row items-center gap-x-5 p-4 rounded-md " + (selected === true ? "bg-orange-500" : "bg-white");
 
     return (
-        <TouchableWithoutFeedback onPress={() => {
-            if (selected === true) {
-                setSelected(false);
-                props.setContact(null);
-                return;
-            }
-            if(selected === false) {
-                if(props.isContactSet === true) {
-                    
-                    return;
-                }
-            }
-            props.setContact({
-                name: props.name,
-                phoneNumbers: [{
-                    id: props.id,
-                    label: props.name,
-                    number: props.phone ?? "",
-                }],
-                imageAvailable: props.image ? true : false,
-                contactType: Contacts.ContactTypes.Person,
-                id: props.id,
-            })
-            setSelected(true);
-        }}>
-            <View className={styles1}>
-                <Image source={props.image ? { uri: props.image } : require("../assets/not_found.png")} alt="Not Found" style={{
-                    width: 30,
-                    height: 30,
-                    resizeMode: "contain",
-                    borderRadius: 50
-                }} />
-                <View className='flex flex-col justify-between'>
-                    <Text className='text-lg'>{props.name}</Text>
-                    <Text className='text-gray-500'>{props.phone ?? ""}</Text>
-                </View>
+        <View className='flex flex-row items-center gap-x-5 p-4 rounded-md'>
+            <Image source={props.image ? { uri: props.image } : require("../assets/not_found.png")} alt="Not Found" style={{
+                width: 30,
+                height: 30,
+                resizeMode: "contain",
+                borderRadius: 50
+            }} />
+            <View className='flex flex-col justify-between'>
+                <Text className='text-lg'>{props.name}</Text>
+                <Text className='text-gray-500'>{props.phone ?? ""}</Text>
             </View>
-        </TouchableWithoutFeedback>
+        </View>
     )
 }

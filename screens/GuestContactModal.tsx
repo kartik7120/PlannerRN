3d6de2bae08cf1c7f1bc061406ddb7e7945887b3
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-07-17 13:59:07 +0530

    added handleSelect function to add contacts to the guest list

--- screens/GuestContactModal.tsx
+++ screens/GuestContactModal.tsx
@@ -3,11 +3,24 @@ import React, { useEffect } from 'react'
 import * as Contacts from 'expo-contacts';
 import ContactNumber from '../components/ContactNumber';
 import { Button } from '@rneui/themed';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStack } from '../App';
+
+type NProps = StackNavigationProp<RootStack, 'GuestContactModal'>;
 
 export default function GuestContactModal() {
 
     const [contacts, setcontacts] = React.useState<Contacts.Contact[] | null>(null);
     const [setselected, setSetselected] = React.useState<Contacts.Contact | null>();
+    const [isLongPressed, setIsLongPressed] = React.useState(false);
+    const [selectedItems, setSelectedItems] = React.useState<any[] | null>(null);
+    const [backgroundStyles, setBackgroundStyles] = React.useState({
+        backgroundColor: "orange",
+        opacity: 0.5
+    });
+    const navigation = useNavigation<NProps>();
 
     useEffect(() => {
         (async () => {
@@ -40,14 +53,52 @@ export default function GuestContactModal() {
         setSetselected(contact);
     }, [])
 
+    const handleContacts = async () => {
+        if (selectedItems === null) {
+            return;
+        }
+        selectedItems.map((contact: Contacts.Contact) => {
+            navigation.push("GuestModal", {
+                firstname: contact.firstName,
+                lastname: contact.lastName,
+                phone: contact.phoneNumbers ? contact.phoneNumbers[0].number : "",
+            })
+        })
+        setSelectedItems(null);
+    }
+
     return (
         <ScrollView className='h-full'>
             <View className='flex flex-col gap-y-4 m-2'>
                 {contacts?.map((contact, index) => {
-                    return <ContactNumber isContactSet={isContactSet} id={contact.id} setContact={handleSelect} key={index} name={contact.name ? contact.name : ""} phone={contact.phoneNumbers ? contact.phoneNumbers[0].number : ""} image={contact.imageAvailable ? contact.image?.uri : null} />
+                    return (
+                        <TouchableOpacity style={{
+                            backgroundColor: selectedItems && selectedItems.includes(contact) ? backgroundStyles.backgroundColor : 'white',
+                            opacity: selectedItems && selectedItems.includes(contact) ? backgroundStyles.opacity : 1,
+                        }} onPress={() => {
+                            if (selectedItems === null) {
+                                return setSelectedItems((items) => {
+                                    return [contact];
+                                })
+                            } else if (selectedItems.includes(contact)) {
+                                return setSelectedItems((todos) => {
+                                    return todos!.filter((todo) => todo !== contact);
+                                })
+                            } else {
+                                return setSelectedItems((items) => {
+                                    return [...items!, contact];
+                                })
+                            }
+                        }}>
+                            <ContactNumber isContactSet={isContactSet} id={contact.id}
+                                key={index} name={contact.name ? contact.name : ""}
+                                phone={contact.phoneNumbers ? contact.phoneNumbers[0].number : ""}
+                                image={contact.imageAvailable ? contact.image?.uri : null} />
+                        </TouchableOpacity>
+                    )
                 })}
             </View>
-            <Button title="SELECT" onPress={() => { }} />
+            <Button title="SELECT" onPress={handleContacts} />
         </ScrollView>
     )
 }

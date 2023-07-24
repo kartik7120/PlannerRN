1e8acfc1d609eb7dbf354b4cfb3c3794337618d1
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-07-24 16:56:00 +0530

    added functions that do different upload stuff

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -6,16 +6,21 @@ import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStack } from '../App';
 import { doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
-import { db } from '../firebase';
+import { db, storage } from '../firebase';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useForm, Controller } from 'react-hook-form';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { Dropdown } from 'react-native-element-dropdown';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import * as ImagePicker from 'expo-image-picker';
 
 type NavProps = StackNavigationProp<RootStack>;
 
 interface Form {
   name: string;
   time?: Date;
+  budget?: number;
+  currency: string;
 }
 
 const setEventName = async ({ eventId, newName }: any) => {
@@ -40,12 +45,20 @@ export default function Settings() {
   const navigation = useNavigation<NavProps>();
   const [visible1, setVisible1] = useState(false);
   const [visible2, setVisible2] = useState(false);
+  const [visible3, setVisible3] = useState(false);
+  const [visible4, setVisible4] = useState(false);
+  const [visible5, setVisible5] = useState(false);
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+  const [value, setDropDownValue] = useState<any>(null);
+  const [isFocus, setIsFocus] = useState(false);
+  const [image, setImage] = useState<any>(null);
+
   const queryClient = useQueryClient();
   const { control, setValue, getValues } = useForm<Form>({
     defaultValues: {
       name: "",
-      time: new Date()
+      time: new Date(),
+      budget: 0
     }
   })
 
@@ -60,6 +73,8 @@ export default function Settings() {
   if (event.status === "success") {
     if (event.data !== null && event.data !== undefined) {
       if (getValues("name") === "" && getValues("name") !== event.data.name) setValue('name', event.data.name)
+      if (getValues("time") === new Date() && getValues("time") !== event.data.time) setValue('time', event.data.time)
+      if (getValues("budget") === 0 && getValues("budget") !== event.data.budget) setValue('budget', event.data.budget)
     }
   }
 
@@ -79,6 +94,64 @@ export default function Settings() {
     hideTimePicker();
   }
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      uploadImage(result.assets[0].uri)
+    }
+  };
+
+  async function handleBudgetChange() {
+    try {
+      const docRef = doc(db, "events", eventId);
+      updateDoc(docRef, {
+        budget: getValues("budget")
+      })
+      setVisible3(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function handleCurrency() {
+    try {
+      const docRef = doc(db, "events", eventId);
+      updateDoc(docRef, {
+        currency: value
+      })
+      setVisible4(false)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const uploadImage = async (image: any) => {
+    try {
+      //upload image to firebase storage and save the image in firestore
+      const storageRef = ref(storage, `events/${eventId}/images/`);
+      await uploadBytes(storageRef, image).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+        getDownloadURL(snapshot.ref).then(async (url) => {
+          await updateDoc(doc(db, 'events', eventId), {
+            image: url
+          })
+        })
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const changeName = useMutation({
     mutationFn: setEventName,
   })
@@ -94,6 +167,17 @@ export default function Settings() {
           setVisible1(false)
           queryClient.invalidateQueries(['currentEventId'])
         }} title="SAVE" />
+      </Dialog>
+      <Dialog isVisible={visible3} onBackdropPress={() => setVisible3(false)}>
+        <Controller control={control} name="budget" render={({ field: { onChange, onBlur, value } }) => (
+          <Input placeholder="Event Budget" value={value?.toString()} onChangeText={onChange} onBlur={onBlur} />
+        )} />
+        <Button title="Change Budget" onPress={handleBudgetChange} />
+      </Dialog>
+      <Dialog isVisible={visible4} onBackdropPress={() => setVisible4(false)}>
+        <Dropdown labelField="label" valueField='value' value={value} onChange={(value) => setDropDownValue(value)}
+          data={[{ label: "INR", value: "INR" }, { label: "USD", value: "USD" }]} />
+        <Button title="Change Currency" onPress={handleCurrency} />
       </Dialog>
       <Controller
         control={control}
@@ -173,13 +257,15 @@ export default function Settings() {
         <Divider width={1} style={{
           marginVertical: 5
         }} />
-        <View className='flex flex-col justify-center items-start w-full gap-y-3'>
-          <View className='flex flex-col gap-y-1'>
-            <Text>Image</Text>
-            <Text className='text-sm text-gray-500'>Upload the event photo</Text>
+        <TouchableOpacity onPress={pickImage}>
+          <View className='flex flex-col justify-center items-start w-full gap-y-3'>
+            <View className='flex flex-col gap-y-1'>
+              <Text>Image</Text>
+              <Text className='text-sm text-gray-500'>Upload the event photo</Text>
+            </View>
+            {/* <Avatar size='large' /> */}
           </View>
-          {/* <Avatar size='large' /> */}
-        </View>
+        </TouchableOpacity>
         <Divider width={1} style={{
           marginVertical: 5
         }} />
@@ -198,7 +284,7 @@ export default function Settings() {
           marginVertical: 5
         }} />
         <TouchableOpacity onPress={() => {
-          setVisible2(true)
+          showTimePicker()
         }}>
           <View className='flex flex-row justify-between items-center w-full gap-y-3'>
             <View className='flex flex-col gap-y-1'>
@@ -211,33 +297,31 @@ export default function Settings() {
         <Divider width={1} style={{
           marginVertical: 5
         }} />
-        <View className='flex flex-row justify-between items-center w-full gap-y-3'>
-          <View className='flex flex-col gap-y-1'>
-            <Text>Budget</Text>
-            <Text className='text-sm text-gray-500'>Event Budget here</Text>
+        <TouchableOpacity onPress={() => {
+          setVisible3(true)
+        }}>
+          <View className='flex flex-row justify-between items-center w-full gap-y-3'>
+            <View className='flex flex-col gap-y-1'>
+              <Text>Budget</Text>
+              <Text className='text-sm text-gray-500'>{event.data && event.data.budget}</Text>
+            </View>
+            <AntDesign name="right" size={20} color="black" />
           </View>
-          <AntDesign name="right" size={20} color="black" />
-        </View>
+        </TouchableOpacity>
         <Divider width={1} style={{
           marginVertical: 5
         }} />
-        <View className='flex flex-row justify-between items-center w-full gap-y-3'>
-          <View className='flex flex-col gap-y-1'>
-            <Text>Currency</Text>
-            <Text className='text-sm text-gray-500'>Event Currency here</Text>
+        <TouchableOpacity onPress={() => {
+          setVisible4(true)
+        }}>
+          <View className='flex flex-row justify-between items-center w-full gap-y-3'>
+            <View className='flex flex-col gap-y-1'>
+              <Text>Currency</Text>
+              <Text className='text-sm text-gray-500'>{event.data && event.data.currency.value}</Text>
+            </View>
+            <AntDesign name="right" size={20} color="black" />
           </View>
-          <AntDesign name="right" size={20} color="black" />
-        </View>
-        <Divider width={1} style={{
-          marginVertical: 5
-        }} />
-        <View className='flex flex-row justify-between items-center w-full gap-y-3'>
-          <View className='flex flex-col gap-y-1'>
-            <Text>Export</Text>
-            <Text className='text-sm text-gray-500'>Export event data</Text>
-          </View>
-          <Button title="DOWNLOAD" />
-        </View>
+        </TouchableOpacity>
       </View>
       <View>
         <Button title="DELETE YOUR ACCOUNT" />
